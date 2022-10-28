b7e2acd897d4944ab4eaac6dc458b632dc69e9e1
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-10-29 01:58:45 +0530

    Added cards for info if not then normal once and separate footer

---
+++ assets/footer.php
@@ -0,0 +1,25 @@
+<footer class="fixed-bottom custom-footer-css" style="background-color: rgba(0, 0, 0, 0.2)">
+    <div class="row justify-content-center custom-footer-inner">
+        <div class="col-md text-center custom-foot-css align-items">
+            <div class="footer-site-logo mb-4">
+                <a href="#">RestroBook</a>
+            </div>
+            <ul class="list-unstyled nav-links mb-5">
+                <li>
+                    <a aria-current="page" href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="booked.php">Booked</a>
+                </li>
+                <li>
+                    <a href="https://github.com/RS2-Codes/Bluelearn">Repo</a>
+                </li>
+
+            </ul>
+        </div>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © 2022 Copyright:
+            <a class="text-white" href="">RS2 Codes</a>
+        </div>
+    </div>
+</footer>
