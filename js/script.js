ef4fa0f2ccba8348e55972cb5a4669a0b4b292fe
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-10-31 19:27:48 +0530

    feat: added js for login/signup modal

--- js/script.js
+++ js/script.js
@@ -22,3 +22,31 @@ for (let i = 0; i < locChanger.length; i++) {
         //sessionStorage.setItem("user_location", e.target.getAttribute('data_loc_id'));
     }
 }
+
+
+const loginSignupForm = document.getElementById('login-signup-form');
+const loginSignupClose = document.getElementById('login-signup-close');
+
+const loginTrigger = document.getElementById('login-trigger');
+
+loginTrigger.onclick = function() {
+    loginSignupForm.style.display = 'flex';
+}
+
+loginSignupClose.onclick = () => {
+    loginSignupForm.style.display = 'none';
+};
+
+const signupTrigger = document.querySelectorAll(".signup__trigger");
+const signupElement = document.querySelector("#signupForm");
+const signupWrapperElement = document.querySelector("#signup-wrapper");
+const forms = document.querySelectorAll(".form");
+
+const openFormFunc = () => {
+    signupTrigger[0].classList.toggle("is-trigger-hidden");
+    signupElement.classList.toggle("is-form-open");
+    signupWrapperElement.classList.toggle("is-wrapper-open");
+};
+
+signupTrigger.forEach((s) => s.addEventListener("click", openFormFunc));
+forms.forEach((f) => f.addEventListener("submit", (e) => e.preventDefault()));
